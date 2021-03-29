ab83a137c81134d45e1f95081549be7339ad71cf
Yana Dziadkouskaya <yana_dziadkouskaya@gmail.com>
2021-03-29 23:14:37 +0300

    Add SecuriryConfiguration, class Role, Permissions

--- repository/src/main/java/com/epam/esm/entity/Role.java
+++ repository/src/main/java/com/epam/esm/entity/Role.java
@@ -1,5 +1,26 @@
 package com.epam.esm.entity;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
 public enum Role {
-    ADMIN, USER
+    ADMIN(Set.of(Permission.READ, Permission.WRITE)), USER(Set.of(Permission.READ));
+
+    private final Set<Permission> permissions;
+
+    Role(Set<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<Permission> getPermissions() {
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getAuthorities() {
+        return getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+    }
 }
